e386c67318e8ee7784d4a0bf616db7c4b21099a9
Hudsonsue <G00219132@gmit.ie>
2018-02-22 18:23:39 +0000

    attempt 1

--- Eulerfive.py
+++ Eulerfive.py
@@ -1,2 +1,13 @@
 #problem euler 5
-# test
+# 22 Feb 2018
+#
+divisors = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+n=0
+i = 20
+
+while n < len(divisors):
+  if i % n == 0:
+       i = i
+  else:
+       i = i+20
+print (i)
