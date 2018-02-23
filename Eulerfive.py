db956df1f11703b2a0d642253139d55e6da8895c
Susan Hudson <Susan.Hudson@gmit.ie>
2018-02-23 09:56:24 +0000

    changed to use range and while loop

--- Eulerfive.py
+++ Eulerfive.py
@@ -2,10 +2,10 @@
 # 22 Feb 2018
 #
 
-i = 2520
-
-while n in range (2,21):
-  if i % n =!= 0:
-       i = i+20
-  else:
-       print (i)
+i = 20
+while i > 19:  
+  for n in range (2,21):
+    if i % n != 0:
+      print(n)
+  i = i+20
+  print ("i", i)
