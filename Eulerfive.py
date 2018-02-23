98a2ddfac54d27dad1f8c9d5fa511e79b3577236
Susan Hudson <Susan.Hudson@gmit.ie>
2018-02-23 14:49:14 +0000

    Sh

--- Eulerfive.py
+++ Eulerfive.py
@@ -2,10 +2,12 @@
 # 22 Feb 2018
 #
 
-i = 20
-while i > 19:  
+i = 2520
+while i > 9:  
   for n in range (2,21):
     if i % n != 0:
       print(n)
-  i = i+20
+      i = i+20
+    else:
+      print (i)   
   print ("i", i)
