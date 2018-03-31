8e1d8f03fb6411913fa5e313fae1284bbb141f31
Hudsonsue <G00219132@gmit.ie>
2018-03-31 10:54:23 +0100

    add test values

--- factorial.py
+++ factorial.py
@@ -1,6 +1,8 @@
 ## G00219132 Susan Hudson Programming & Scripting
 ## script defines a function to return factorial of entered integers.
 ## https://en.wikipedia.org/wiki/Factorial
+## for positive integer n! is n multiplied by all positive integars less than it
+## example 4! = 4*3*2*1 = 24
 
 def factorial(n):
     if n ==0:
@@ -8,5 +10,8 @@ def factorial(n):
     else:
         return n * factorial (n-1)
 
-n = int(input("Enter an integar: "))
+n = int(input("Enter a positive integar: "))
 print ("the factorial of your integar is", factorial(n))
+
+# Test the function with the following values, 5, 7, 10
+print ("Check script outputs 120 for factorial 5 ---", factorial (5))
