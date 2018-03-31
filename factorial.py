0a46b3e392057b560d617bf7bfa484b54f40fdb0
Hudsonsue <G00219132@gmit.ie>
2018-03-31 10:31:55 +0100

    Factorial function

---
+++ factorial.py
@@ -0,0 +1,6 @@
+## G00219132 Susan Hudson Programming & Scripting
+## script defines a function to return factorial of entered integers.
+## https://en.wikipedia.org/wiki/Factorial
+
+def (factorial)
+    
--- irisformat.py
+++ irisformat.py
@@ -1,7 +1,8 @@
+# iris dataset formatting exercise
+# G00219132 Susan Hudson
+# this script opens the Iris dataset csv file
+# for each line of the dataset it displays the first four values in columns of 5 characters width
+
 with open ("iris.csv") as f:
     for line in f:
-<<<<<<< HEAD:openfile.py
         print ('{:5} {:5} {:5} {:5}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))
-=======
-        print ('{:2} {:2} {:2} {:2}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3], end=' '))
->>>>>>> 09af162ca87f6f26975e4bbbeb8fd1a4a8bb1146:irisformat.py
