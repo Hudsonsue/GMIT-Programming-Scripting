e6f94f2c3a9a94f620a41cc07c1f7a8e0d46c015
Hudsonsue <G00219132@gmit.ie>
2018-04-02 13:51:01 +0100

    factorial function

--- factorial.py
+++ factorial.py
@@ -1,13 +1,18 @@
-## G00219132 Susan Hudson Programming & Scripting
-## script defines a function to return factorial of entered integers.
-## https://en.wikipedia.org/wiki/Factorial
-## for positive integer n! is n multiplied by all positive integars less than it
-## example 4! = 4*3*2*1 = 24
-## 0! = 1   
+# G00219132 Susan Hudson Programming & Scripting
+# script defines a function to return factorial of entered integers.
+# https://en.wikipedia.org/wiki/Factorial
+# for positive integer n! is n multiplied by all positive integars less than it
+# example 4! = 4*3*2*1 = 24
+# 0! = 1   
 
 def factorial(n):
-    if n ==0:
+    """This function returns factorial of positive integar n."""
+    """where n = 0 will return 1 as 0! = 1. Where n < 0 returns Invalid argument warning"""
+    
+    if n == 0:
         return 1
+    elif n < 0:
+        return print('INVALID ARGUMENT, Positive integars only please!!')
     else:
         return n * factorial (n-1)
 
@@ -15,8 +20,6 @@ n = int(input("Enter a positive integar: "))
 print ("the factorial of your integar is", factorial(n))
 
 # Test the function with the following values, 5, 7, 10
-print ("Check script outputs 120 for factorial 5 ---", factorial (5))
-print ("Check script outputs 5040 for factorial 7 ---", factorial (7))
-print ("Check script outputs 3628800 for factorial 10 ---", factorial (10))
-
-# output as expected for test
+print ("the factorial of 5 is:", factorial (5))
+print ("the factorial of 7 is:", factorial (7))
+print ("the factorial of 10 is:", factorial (10))
