60d121a72fb9e6515fc83b3f5d9a9ae9ef69a282
Hudsonsue <G00219132@gmit.ie>
2018-04-06 21:17:20 +0100

    change loop around

--- Eulerfive.py
+++ Eulerfive.py
@@ -4,16 +4,18 @@
 #+= adds a number to a variable,
 # changing the variable itself in the process (whereas + would not). Similar to this, there are the following that also modifies the variable:
 
-i=0
+i=2520
 
-while True:  #continues looping until 
-  i = 2520 #+= is incrementing by 20 each time it loops, has to be divisible by 20 so no point smaller increments
-  for n in range (2,21):
-    if i % n != 0:break #stops going through n values when a value of n results in non even division
-    i=i+20
-    print (n) #temp to check it is looping correctly
-    print(i) #temp to check it is looping correctly#temp
-  else:    
-    print ("i", i)  
+for n in range (2,21):
+  if i % n == 0:
+    continue #stops going through n values when a value of n results in non even division
+  else:
+      i=i+20
+    break
+  if  n ==20:
+    answer = n
+print (answer)  
+
+
 
     
