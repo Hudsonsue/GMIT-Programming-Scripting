27a8a2e976cebdc17f87daa51ba1c421a77e92f9
Hudsonsue <G00219132@gmit.ie>
2018-04-06 18:35:15 +0100

    changed dataset see comments

--- irisformat.py
+++ irisformat.py
@@ -1,10 +1,17 @@
 # iris dataset formatting exercise 6
-# G00219132 Susan Hudson 
+# G00219132 Susan Hudson
 # this script opens the Iris dataset csv file
 # for each line of the dataset it displays the first four values in columns of 5 characters width
 
-with open ("iris.csv") as f:
-    for line in f:
-        print ('{:5} {:5} {:5} {:5}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))
-        
-        #whilst this returns data it does also contain an index error
+with open ("irisdata.csv") as f:
+
+     for line in f:
+
+         print ('{:5} {:5} {:5} {:5}'.format(line.split (';')[0], line.split (';')[1], line.split(';')[2], line.split (';')[3]))
+
+         ## note - had been getting index error messages when running, attempted to tidy up at work but did not have access to dataset. 
+         ## downloaded dataset again from a different source and re wrote script printing unformatted, column by column then formatting
+         ## no index errors so am assuming problem was within original dataset not my script
+         ## this csv is ; seperated not 
+
+         ## www.saedsayad.com/datasets/Iris.xls, stripping out summaries and saving as csv
