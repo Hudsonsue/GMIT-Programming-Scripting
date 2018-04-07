776c97a8c2145736e08a4ba667503ff80fbbd0a4
Hudsonsue <G00219132@gmit.ie>
2018-04-07 12:01:59 +0100

    collatz final

--- Collatz.py
+++ Collatz.py
@@ -1,9 +1,14 @@
 # Susan Hudson Collatz exercise
+# week 3 exercise programming & scripting
 # Collatz: https://en.wikipedia.org/wiki/Collatz_conjecture
+# the exercise was completed by reference to lecture videos and the Python tutorial https://docs.python.org/3/tutorial/
+
 
 n = int(input("enter an integer: "))
 
-print (n)
+print ("The integer entered is",n)
+print ("The collatz sequence of",n,"is")
+
 while n >1:
     if n % 2 == 0:
             n = n / 2  
