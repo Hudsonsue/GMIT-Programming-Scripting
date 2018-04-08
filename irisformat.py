3e95be82ec056e1a0f5cf9c6eb6f442c53a1f79c
Hudsonsue <G00219132@gmit.ie>
2018-04-08 20:08:31 +0100

    irisformat final formatted

--- irisformat.py
+++ irisformat.py
@@ -3,6 +3,7 @@
 # this script opens the Iris dataset csv file
 # for each line of the dataset it displays the first four values in columns of 5 characters width
 # I played around with right or left justifying (< and >), no huge difference as same number of characters in each column
+# have centred as csv includes headers
 # what I wanted to do was not have to split out each column with format line split but couldn't, hence the long print command
 # https://pyformat.info/ and python tutorial used as well as lecture videos
 
@@ -10,7 +11,7 @@ with open ("irisdata.csv") as f:
 
      for line in f:
 
-         print ('{:>5} {:>5} {:>5} {:>5}'.format(line.split (',')[0], line.split (',')[1], line.split(',')[2], line.split (',')[3]))
+         print ('{:^12} {:^12} {:^12} {:^12}'.format(line.split (',')[0], line.split (',')[1], line.split(',')[2], line.split (',')[3]))
 
          ## note - had been getting index error messages when running, attempted to tidy up at work but did not have access to dataset. 
          ## downloaded dataset again from a different source and re wrote script printing unformatted, column by column then formatting
