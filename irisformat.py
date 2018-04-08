cf63cab932e97bace4eb684cc54feb4814d91ee7
Hudsonsue <G00219132@gmit.ie>
2018-04-08 20:11:13 +0100

    iris format final formatted

--- irisformat.py
+++ irisformat.py
@@ -13,8 +13,8 @@ with open ("irisdata.csv") as f:
 
          print ('{:^12} {:^12} {:^12} {:^12}'.format(line.split (',')[0], line.split (',')[1], line.split(',')[2], line.split (',')[3]))
 
-         ## note - had been getting index error messages when running, attempted to tidy up at work but did not have access to dataset. 
-         ## downloaded dataset again from a different source and re wrote script printing unformatted, column by column then formatting
-         ## no index errors so am assuming problem was within original dataset not my script
+## note - had been getting index error messages when running, attempted to tidy up at work but did not have access to dataset. 
+## downloaded dataset again from a different source and re wrote script printing unformatted, column by column then formatting
+## no index errors so am assuming problem was within original dataset not my script
     
-         ## used data set from www.saedsayad.com/datasets/Iris.xls, stripping out summaries and saving as csv
+## used data set from www.saedsayad.com/datasets/Iris.xls, stripping out summaries and saving as csv
